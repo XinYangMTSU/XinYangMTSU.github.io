0b7066f0a81a2942eacbc02689fe7ceb88aa6a7b
Xin Yang <yangxinncut@gmail.com>
2024-01-10 20:56:32 -0600

    Add files via upload

---
+++ CSS/Script.js
@@ -0,0 +1,17 @@
+document.getElementById("foot01").innerHTML =
+	"<p>&copy;  " + new Date().getFullYear() + " Xin Yang. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+	"<ul id='menu'>" +
+	"<li><a href='index.html'>Home</a></li>" +
+	"<li><a href='2170.html'>CSCI 2170</a></li>" +
+	"<li><a href='4410.html'>CSCI 4410/5410</a></li>" +
+	"<li><a href='3080.html'>CSCI 3080</a></li>" +
+	"<li><a href='MySchedule.html'>Schedule</a></li>" +
+	"<li><a href='MyResearch.html'>Research</a></li>" +
+	"<li><a href='https://www.mtsu.edu/faculty/xin-yang' target='_blank'>CV</a></li>" +
+	"<li><a href='MyDrawings.html'>Drawings</a></li>" +
+	"<li><a href='FAQ.html'>FAQ</a></li>" +
+	"<li><a href='https://www.cs.mtsu.edu/cgi-bin/gus/gus.py' target='_blank'>Gus</a></li>" +
+	"<li><a href='StudentsDemo.html' target='_blank'>StudentDemo</a></li>"
+"</ul>";
+
