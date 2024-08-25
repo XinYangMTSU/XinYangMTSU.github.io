2bf208603314f47b0868940bdf09db9a68577376
Xin Yang <yangxinncut@gmail.com>
2024-08-24 22:16:31 -0500

    Update Script.js

--- CSS/Script.js
+++ CSS/Script.js
@@ -3,8 +3,6 @@ document.getElementById("foot01").innerHTML =
 document.getElementById("nav01").innerHTML =
 	"<ul id='menu'>" +
 	"<li><a href='index.html'>Home</a></li>" +
-	"<li><a href='2170.html'>CSCI 2170</a></li>" +
-	"<li><a href='4410.html'>CSCI 4410/5410</a></li>" +
 	"<li><a href='3080.html'>CSCI 3080</a></li>" +
 	"<li><a href='MySchedule.html'>Schedule</a></li>" +
 	"<li><a href='MyResearch.html'>Research</a></li>" +
@@ -13,6 +11,7 @@ document.getElementById("nav01").innerHTML =
 	"<li><a href='FAQ.html'>FAQ</a></li>" +
 	"<li><a href='StudentsDemo.html' target='_blank'>StudentDemo</a></li>"
 "</ul>";
-
+// "<li><a href='2170.html'>CSCI 2170</a></li>" +
+// "<li><a href='4410.html'>CSCI 4410/5410</a></li>" +
 // "<li><a href='https://www.cs.mtsu.edu/cgi-bin/gus/gus.py' target='_blank'>Gus</a></li>" 
 
