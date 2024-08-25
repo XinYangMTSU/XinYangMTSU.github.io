a449ad073b64ee813ae2d517186ab97de0f87d88
Xin Yang <yangxinncut@gmail.com>
2024-08-24 22:17:24 -0500

    Update Script.js

--- CSS/Script.js
+++ CSS/Script.js
@@ -8,10 +8,12 @@ document.getElementById("nav01").innerHTML =
 	"<li><a href='MyResearch.html'>Research</a></li>" +
 	"<li><a href='https://www.mtsu.edu/faculty/xin-yang' target='_blank'>CV</a></li>" +
 	"<li><a href='MyDrawings.html'>Drawings</a></li>" +
-	"<li><a href='FAQ.html'>FAQ</a></li>" +
+	
 	"<li><a href='StudentsDemo.html' target='_blank'>StudentDemo</a></li>"
 "</ul>";
+
 // "<li><a href='2170.html'>CSCI 2170</a></li>" +
 // "<li><a href='4410.html'>CSCI 4410/5410</a></li>" +
+// "<li><a href='FAQ.html'>FAQ</a></li>" +
 // "<li><a href='https://www.cs.mtsu.edu/cgi-bin/gus/gus.py' target='_blank'>Gus</a></li>" 
 
