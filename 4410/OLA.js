efa980c65b416ebaa1ddc3ffdc72b62dca55f523
Xin Yang <yangxinncut@gmail.com>
2025-02-06 08:25:57 -0600

    Create OLA.js

---
+++ 4410/OLA.js
@@ -0,0 +1,8 @@
+let goTopBtn = document.getElementById("goTopBtn");
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+        
+goTopBtn.onclick = topFunction;
