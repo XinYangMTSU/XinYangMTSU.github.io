884904cfbd7d91d101edf060d65efe669df665b3
Xin Yang <yangxinncut@gmail.com>
2025-02-11 09:04:37 -0600

    Update OLA.js

--- 4410/OLA.js
+++ 4410/OLA.js
@@ -1,8 +1,13 @@
+ // 1. Get a reference to the element.
 let goTopBtn = document.getElementById("goTopBtn");
 
+// 2. Define the function to scroll to the top.
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
         
-goTopBtn.onclick = topFunction;
+//goTopBtn.onclick = topFunction;
+
+ // 3. Use addEventListener to bind the click event to topFunction.
+  goTopBtn.addEventListener('click', topFunction);
