9ea99bdc13d602b95828ad9df992c554f18d267f
Xin Yang <yangxinncut@gmail.com>
2025-03-24 23:12:20 -0500

    Create show-code.php

---
+++ 4410/show-code.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View contact.php Source Code</title>
+</head>
+<body>
+    <h1>📄 Source Code: contact.php</h1>
+    <pre>
+    <?php
+        highlight_file('contact.php');
+    ?>
+    </pre>
+</body>
+</html>
